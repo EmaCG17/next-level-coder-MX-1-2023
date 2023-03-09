5e76206a9bb1234e108ac01097f663edd2833bf4
EmaCG17 <emanuel.de.jesus.cruz.gomez.cb113@gmail.com>
2023-03-08 21:35:31 -0600

    class 5: Power_ups

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,8 +1,20 @@
 import pygame
-from dino_runner.utils.constants import (BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS,FONT_ARIAL)
+
+
+from dino_runner.utils.constants import (
+    BG, 
+    ICON, 
+    SCREEN_HEIGHT, 
+    SCREEN_WIDTH, 
+    TITLE, 
+    FPS,
+    FONT_ARIAL
+    )
+from dino_runner.components.dinosaur import Dinosaur 
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
-from dino_runner.components.dinosaur import Dinosaur
+
 from dino_runner.components.player_hearts.heart_manager import HeartManager
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
 
 class Game:
     def __init__(self):
@@ -15,16 +27,18 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
-        self.player = Dinosaur()
-        self.obstacles_manager = ObstacleManager()
+
+        self.player = Dinosaur() #dinosaurio
+        self.obstacle_manager = ObstacleManager() #obstacleManager
         self.heart_manager = HeartManager()
+        self.power_up_manager = PowerUpManager()
         self.points = 0
 
     def increase_score(self):
         self.points += 1
         if self.points % 100 == 0:
             self.game_speed += 1
-        
+        self.player.check_invincibility()
 
     def run(self):
         # Game loop: events - update - draw
@@ -41,30 +55,34 @@ class Game:
                 self.playing = False
 
     def update(self):
+        pass
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-        self.obstacles_manager.update(self.game_speed, self)
+        self.obstacle_manager.update(self.game_speed, self)
+        self.power_up_manager.update(self.points, self.game_speed, self.player)
         self.increase_score()
 
-
+    
     def draw(self):
-        self.clock.tick(FPS)
-        if self.points < 500:
-           self.screen.fill((255, 255, 255))
-        else:
-            self.screen.fill((0, 0, 0))
-        self.player.draw(self.screen)
-        self.draw_score()
-        self.obstacles_manager.draw(self.screen)
-        self.heart_manager.draw(self.screen)
-        self.draw_background()
-        if self.heart_manager.heart_count == 1:
-            self.draw_final()
-        elif self.heart_manager.heart_count == 1 and self.points > 500:
-            self.draw_final()
-        pygame.display.update()
-        pygame.display.flip()
 
+       if self.heart_manager.heart_count > 0:
+            self.clock.tick(FPS)
+            if self.points < 1000:
+                self.screen.fill((255, 255, 255))
+            else:
+                self.screen.fill((0, 0, 0))
+            
+            self.draw_background()
+            self.player.draw(self.screen)
+            self.obstacle_manager.draw(self.screen)
+            self.draw_score()
+            self.power_up_manager.draw(self.screen)
+            self.heart_manager.draw(self.screen)
+
+            pygame.display.update() #update objects inside
+            pygame.display.flip() #display/show
+       else:
+        pass
 
     def draw_background(self):
         image_width = BG.get_width()
@@ -77,24 +95,14 @@ class Game:
 
     def draw_score(self):
         font = pygame.font.Font(FONT_ARIAL, 30)
-        if self.points < 500:
-            surface = font.render("High Score: "+ str(self.points), True, (0,0,0))
+        if self.points < 1000:
+            surface = font.render(str(self.points), True, (0, 0, 0))
         else:
-             surface = font.render("High Score: "+ str(self.points), True, (255,255,255))
+            surface = font.render(str(self.points), True, (255, 255, 255))
         rect = surface.get_rect()
-        rect.x = 900
+        rect.x = 1000
         rect.y = 10
         self.screen.blit(surface, rect)
-
-    def draw_final(self):
-        font = pygame.font.Font(FONT_ARIAL, 30)
-        surface = font.render("Te queda una vida", True, (0,0,0))
-       # time = pygame.time.get_ticks() / 1000
-        rect = surface.get_rect()
-        rect.x = 450
-        rect.y = 300
-        self.screen.blit(surface, rect)
-
     
 
     
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -2,35 +2,38 @@ import pygame
 import random
 from dino_runner.components.obstacles.captus import Captus
 from dino_runner.components.obstacles.bird import Bird
-from dino_runner.utils.constants import SMALL_CACTUS,BIRD
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS,BIRD
 
 class ObstacleManager:
 
     def __init__(self):
         self.obstacles = []
 
-    def update(self,game_speed,game):
+    def update(self, game_speed, game):
+
         if len(self.obstacles) == 0:
-            if random.randint(0, 1) == 0:
-                self.obstacles.append(Captus(SMALL_CACTUS))
-            elif random.randint(0, 1) == 1:
-                self.obstacles.append(Bird(BIRD))
+            type = random.randint(0,2)
+            match type:
+                case 0:
+                    self.obstacles.append(Bird(BIRD))
+                case 1:
+                    self.obstacles.append(Captus(SMALL_CACTUS))
+                case 2:
+                    self.obstacles.append(Captus(LARGE_CACTUS))
 
         for obstacle in self.obstacles:
             obstacle.update(game_speed, self.obstacles)
 
             if game.player.dino_rect.colliderect(obstacle.rect):
-                game.heart_manager.reduce_heart()
+                if not game.player.shield:
+                    game.heart_manager.reduce_heart()
                 if game.heart_manager.heart_count < 1:
-                   pygame.time.delay(300)
-                   game.playing = False
-                   break
-                else:
-                    self.obstacles.remove(obstacle)
+                    pygame.time.delay(300)
+                    game.playing = False
                     
-          
+                else:
+                   self.obstacles.remove(obstacle)
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
-
